ad598100ea8f65b2a94205925d204a95dab6824b
Sani Omotola <noble.b.sage01@gmail.com>
2025-06-14 11:12:18 +0100

    migrated user file

--- shop/views.py
+++ shop/views.py
@@ -8,3 +8,14 @@ def index(request):
         'food_items': Food.objects.all()
     }
     return render(request, 'index.html', context)
+
+def food_detail(request, food_id):
+    try:
+        food_item = Food.objects.get(id=food_id)
+    except Food.DoesNotExist:
+        return render(request, '404.html', status=404)
+
+    context = {
+        'food_item': food_item
+    }
+    return render(request, 'detail.html', context)
